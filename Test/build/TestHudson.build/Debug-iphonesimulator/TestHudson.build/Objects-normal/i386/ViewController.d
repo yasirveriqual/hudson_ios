e6d97eac66d6394c6fa533def356dcd71168c86d
Yasir Ali <yasiraliraj@gmail.com>
2012-05-11 14:47:42 +0500

    commiting unit tests

---
+++ Test/build/TestHudson.build/Debug-iphonesimulator/TestHudson.build/Objects-normal/i386/ViewController.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/ViewController.m \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/TestHudson-Prefix.pch \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/ViewController.h
---
+++ Test/build/TestHudson.build/Debug-iphonesimulator/TestHudson.build/StaticAnalyzer/normal/i386/main.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/main.m \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/TestHudson-Prefix.pch \
+  /Volumes/Data\ HD/Projects/iOS/Learning/Hudson/hudson_ios/Test/TestHudson/AppDelegate.h
